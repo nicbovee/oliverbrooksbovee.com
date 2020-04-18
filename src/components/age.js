148ee8bebc69e3035b93803ca32b6c14786f2688
Nicolas Bovee <admin@ghettifish.com>
2020-04-17 20:12:49 -0600

    Try to fix build by changing case on react,

--- src/components/age.js
+++ src/components/age.js
@@ -4,10 +4,11 @@ import {fonts, colors} from "../utils/styles";
 
 const BIRTHDAY =  new Date(2020, 3, 10, 9, 9, 0 , 0);
 const MS_IN_YEAR = 31536000000;
-const MS_IN_MIN = 60000;
-const MS_IN_HR = MS_IN_MIN * 60;
-const MS_IN_DAY = MS_IN_HR * 24;
 const MS_IN_MONTH = MS_IN_DAY * 30;
+const MS_IN_WEEK = (MS_IN_HR * 24) * 7;
+const MS_IN_DAY = MS_IN_HR * 24;
+const MS_IN_HR = MS_IN_MIN * 60;
+const MS_IN_MIN = 60000;
 const MS_IN_HEARTBEAT = 500;
 const MS_IN_BREATH = 1333;
 
@@ -16,16 +17,18 @@ const getCurrentAgeMinutes = () => Math.floor(getCurrentAge() / MS_IN_MIN)
 const calculateAge = () => {
     let current = getCurrentAge();
     let years = Math.floor(current/MS_IN_YEAR);
-    let days = Math.floor(current/MS_IN_DAY);
+    let weeks = Math.floor(current/MS_IN_WEEK);
+    let days = Math.floor((current%MS_IN_WEEK)/MS_IN_DAY);
     let hours = Math.floor((current%MS_IN_DAY)/MS_IN_HR);
     let minutes = Math.floor((current%MS_IN_HR) / MS_IN_MIN);
 
     let stringYears = years ? `${years} year${years > 1 ? 's' : ''} ` : '';
+    let stringWeeks = weeks ? `${weeks} week${weeks > 1 ? 's' : ''}` : '';
     let stringDays = days ? `${days} day${days > 1 ? 's' : ''}` : '';
     let stringHours = hours ? `${hours} hour${hours > 1 ? 's' : ''}` : '';
     let stringMinutes = minutes ? `${minutes} minute${minutes > 1 ? 's' : ''}`: '';
     
-    return `${stringYears} ${stringDays} ${stringHours} ${stringMinutes ? "and " + stringMinutes : stringMinutes} old`
+    return `${stringYears} ${stringWeeks} ${stringDays} ${stringHours} ${stringMinutes ? "and " + stringMinutes : stringMinutes} old`
 }
 
 const calculateHeartBeats = () =>  (Math.floor(getCurrentAge() / MS_IN_HEARTBEAT));
